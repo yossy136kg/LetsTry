dbd71fb67515b86fe1f8fcfee7d7496f70881393
yossy <=>
2022-03-22 11:20:36 +0900

    2

--- src/self/chatchup/capter1.java
+++ src/self/chatchup/capter1.java
@@ -4,6 +4,8 @@ public class capter1 {
     public static void main(String[] args) {
         System.out.println("hello1");
         System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
 
     }
 }
