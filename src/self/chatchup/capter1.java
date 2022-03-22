b82905abf19d00058df3f591b19658bff3755acf
yossy <=>
2022-03-22 11:31:08 +0900

    2

--- src/self/chatchup/capter1.java
+++ src/self/chatchup/capter1.java
@@ -3,7 +3,7 @@ package self.chatchup;
 public class capter1 {
     public static void main(String[] args) {
         System.out.println("hello1");
-        System.out.println("hello");
+        System.out.println("hello2");
         System.out.println("hello");
         System.out.println("hello");
         System.out.println("hello");
