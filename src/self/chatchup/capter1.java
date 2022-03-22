254dabb461dcf757ad47e244a59e7e699e8f2dc3
yossy <=>
2022-03-22 11:31:55 +0900

    4

--- src/self/chatchup/capter1.java
+++ src/self/chatchup/capter1.java
@@ -5,7 +5,7 @@ public class capter1 {
         System.out.println("hello1");
         System.out.println("hello2");
         System.out.println("hello3");
-        System.out.println("hello");
+        System.out.println("hello4");
         System.out.println("hello");
         System.out.println("hello");
 
